618b00a814197a5c2c2d356c43d4d3a47a0a11bc
joshmagolo <joshua.magolo@students.moringaschool.com>
2026-07-18 14:24:30 +0300

    Create CashRegister class

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,60 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity=1):
+        # Add price to total
+        self.total += price * quantity
+
+        # Add item names to items list
+        for i in range(quantity):
+            self.items.append(item)
+
+        # Store transaction details
+        self.previous_transactions.append({
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        })
+
+    def apply_discount(self):
+        if self.discount == 0:
+            print("There is no discount to apply.")
+            return
+
+        self.total = self.total - (self.total * self.discount / 100)
+
+        print(f"After the discount, the total comes to ${self.total:.0f}.")
+
+    def void_last_transaction(self):
+        if len(self.previous_transactions) == 0:
+            return
+
+        last_transaction = self.previous_transactions.pop()
+
+        item = last_transaction["item"]
+        price = last_transaction["price"]
+        quantity = last_transaction["quantity"]
+
+        # Remove items
+        for i in range(quantity):
+            self.items.remove(item)
+
+        # Reduce total
+        self.total -= price * quantity
